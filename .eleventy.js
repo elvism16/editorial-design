62bcc58d7228e3f626fee5797331c9bdc5ae1e47
Marc Mucha <marc.mucha@bitgrip.de>
2020-03-23 09:59:52 +0100

    updating files
    updating files

--- .eleventy.js
+++ .eleventy.js
@@ -2,5 +2,5 @@ const sass = require("./config/sass-process");
 
 module.exports = config => {
   //Watching for modificaions in style directory
-  sass("./scss/styles.scss", "./_site/style/styles.css");
+  sass("./scss/styles.scss", "./_site/css/styles.css");
 };
